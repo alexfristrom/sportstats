5ae91b6c4e955432b3ab88243d2742d414d34852
17dawa01 <17dawa01@12314-STUD-D.HIG-AD.hig.se>
2022-04-20 10:51:23 +0200

    git ignore commit

---
+++ Domain/src/main/java/sportstats/domain/Match.java
@@ -0,0 +1,104 @@
+package sportstats.domain;
+
+import com.fasterxml.jackson.annotation.*;
+import java.util.Date;
+import javax.persistence.*;
+/**
+ * hometeam_id, awayteam_id, season_id, result_id, round, date, spectators
+ *
+ * @author danielw
+ */
+@Entity
+@JsonIgnoreProperties("hibernateLazyInitializer")
+public class Match implements Listable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private Long hometeam_id;
+    
+    private Long awayteam_id;
+    
+    private Long season_id;
+    
+    private Long result_id;
+    
+    private Byte round;
+    
+    private Date date;
+    
+    private Long spectators;
+    
+    public Match() {
+    }
+
+    public Match(Long id) {
+        this.id = id;
+        
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    
+    public Long getHomeTeamId() {
+        return hometeam_id;
+    }
+
+    public void setHomeTeamId(Long hometeam_score) {
+        this.hometeam_id = hometeam_score;
+    }
+    
+    public Long getAwayTeamId() {
+        return awayteam_id;
+    }
+
+    public void setAwayTeamId(Long awayteam_score) {
+        this.awayteam_id = awayteam_id;
+    }
+    
+    public Long getMatchSeasonId() {
+        return season_id;
+    }
+
+    public void setMatchSeasonId(Long season_id) {
+        this.season_id = season_id;
+    }
+    
+    public Long getMatchResultId() {
+        return result_id;
+    }
+
+    public void setMatchResultId(Long result_id) {
+        
+        this.result_id = result_id;
+    }
+    
+    public Byte getMatchRound() {
+        return round;
+    }
+
+    public void setMatchRound(Byte round) {
+        this.round = round;
+    }
+    
+    public Date getMatchDate() {
+        return date;
+    }
+
+    public void setMatchDate(Date date) {
+        this.date = date;
+    }
+
+    public Long getMatchSpectators() {
+        return spectators;
+    }
+
+    public void setMatchSpectators(Long spectators) {
+        this.spectators = spectators;
+    }
+    
+}
