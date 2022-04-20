04fbdf7bee14645c5776421f662eed09e909e6fc
alexf <alexf@193.11.63.85>
2022-04-20 11:10:46 +0200

    Merge origin/master
    Conflicts:
    	Domain/src/main/java/test/domain/League.java
    	Rest/src/main/java/test/TestAplication.java

---
+++ Domain/src/test/java/SeasonTest.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test for Domain object Season
+ * @author alexf
+ */
+public class SeasonTest {
+    
+    @Test
+    public void 
+    
+}
