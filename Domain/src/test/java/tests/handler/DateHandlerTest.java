52ed9044428446cc7fb721890622f38c5ff86da1
alexf <alexf@193.11.63.85>
2022-04-21 15:20:52 +0200

    Merge origin/master into seasonJunit
    Conflicts:
    	.gitignore
    	Rest/src/resources/application.properties
    	nb-configuration.xml

---
+++ Domain/src/test/java/tests/handler/DateHandlerTest.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package tests.handler;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import sportstats.handler.DateHandler;
+
+/**
+ * Test for handler of LocalDate/Time Involves test for Metohds for LocalDate,
+ * LocalTime and LocalDateTime
+ *
+ * @author alexf
+ */
+public class DateHandlerTest {
+
+    @Test
+    public void testDate() {
+        DateHandler handler = new DateHandler();
+        handler.addDate(2022, 5, 15);
+        LocalDate date = handler.getDate();
+
+        LocalDate refferenceDate = LocalDate.of(2022, 5, 15);
+        assertEquals(refferenceDate, date);
+
+        System.out.println("Date: " + date);
+
+    }
+
+    @Test
+    public void testTime() {
+        DateHandler handler = new DateHandler();
+        handler.addTime(20, 15, 0);
+        LocalTime time = handler.getTime();
+
+        LocalTime refferenceTime = LocalTime.of(20, 15, 0);
+        assertEquals(refferenceTime, time);
+
+        System.out.println("Time: " + time);
+    }
+
+    @Test
+    public void testDateTime() {
+        DateHandler handler = new DateHandler();
+        handler.addTime(20, 15, 0);
+        handler.addDate(2022, 5, 15);
+        LocalDateTime dateTime = handler.getDateTime();
+
+        LocalDateTime refferenceDateTime = LocalDateTime.of(2022, 5, 15, 20, 15, 0);
+        assertEquals(refferenceDateTime, dateTime);
+
+        System.out.println("Date and Time :" + dateTime);
+
+    }
+
+    @Test
+    public void getDateExcludeTimeFromDateTime() {
+        DateHandler handler = new DateHandler();
+        handler.addTime(20, 15, 0);
+        handler.addDate(2022, 5, 15);
+        LocalDateTime dateTime = handler.getDateTime();
+        
+        LocalDate refferenceDate = LocalDate.of(2022, 5, 15);
+        LocalDate date = dateTime.toLocalDate();
+        assertEquals(refferenceDate, date);
+        
+        System.out.println("Date only from DateTime: " + date);
+    }
+    
+    @Test
+    public void getTimeExcludeDateFromDateTime(){
+        DateHandler handler = new DateHandler();
+        handler.addTime(20, 15, 0);
+        handler.addDate(2022, 5, 15);
+        LocalDateTime dateTime = handler.getDateTime();
+        
+        LocalTime refferenceTime = LocalTime.of(20, 15, 0);
+        LocalTime time = dateTime.toLocalTime();
+        assertEquals(refferenceTime, time);
+        
+        System.out.println("Time only from DateTime: " + time);
+    }
+    
+    @Test
+    public void test(){
+        
+    }
+
+}
