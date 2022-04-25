6a84d9fafd643dd6d29d576aa0ba025e235f8632
alexf <alexf@193.11.63.85>
2022-04-25 10:29:07 +0200

    Merge resource
    Conflicts:
    	Domain/src/main/java/sportstats/domain/Game.java
    	Repository/nb-configuration.xml
    	Rest/nb-configuration.xml
    	Service/nb-configuration.xml
    	nb-configuration.xml

--- Domain/src/main/java/sportstats/domain/Game.java
+++ Domain/src/main/java/sportstats/domain/Game.java
@@ -1,6 +1,7 @@
 package sportstats.domain;
 
 import com.fasterxml.jackson.annotation.*;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import javax.persistence.*;
 
@@ -11,7 +12,7 @@ import javax.persistence.*;
  */
 @Entity
 @JsonIgnoreProperties("hibernateLazyInitializer")
-public class Game implements Listable {
+public class Game implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
