7ddb1467573e11ef8778946ce96e6edef31cad0a
alexf <alexf@193.11.63.85>
2022-05-05 11:43:06 +0200

    Merge SeasonService
    Conflicts:
    	Domain/src/main/java/sportstats/domain/League.java
    	Rest/src/main/java/sportstats/rest/SportResource.java
    	Rest/src/main/resources/application.properties
    	nb-configuration.xml

--- Service/src/test/java/SeasonTest.java
+++ Service/src/test/java/SeasonTest.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 
+import java.util.ArrayList;
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,7 @@ import org.mockito.Mockito;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.MockitoJUnitRunner;
+import sportstats.domain.Listable;
 
 import sportstats.domain.Sport;
 import sportstats.repository.SportRepository;
@@ -45,7 +48,43 @@ public class SeasonTest {
         sport.setName("Fotboll");
         Mockito.when(sportRepository.save(any(Sport.class))).thenReturn(sport); 
 
-        sportService.saveSport(sport);
-        assertEquals("Fotboll", sport.getName());
+        Sport newSport = sportService.saveSport(sport);
+        assertEquals("Fotboll", newSport.getName());
+    }
+    
+    @Test
+    public void testGetSport(){
+        System.out.println("GetSportTestS");
+        sportRepository = mock(SportRepository.class);
+        sportService = new SportService(sportRepository);
+        
+        Long id = 1l;
+        Sport sport = new Sport(id, "Testname");
+        Mockito.when(sportRepository.getById(id)).thenReturn(sport);
+        
+        Sport methodSport = sportService.getSport(id);
+        
+        assertEquals("Testname", methodSport.getName());
+        assertEquals(id, methodSport.getId());
+        
+    }
+    
+    @Test
+    public void testGetAllSport(){
+        System.out.println("GetAllSportTest");
+        sportRepository = mock(SportRepository.class);
+        sportService = new SportService(sportRepository);
+        
+        Long id = 1l;
+        String name = "testname";
+        Sport sport = new Sport(id, name);
+        List<Sport> sportList = new ArrayList<>();
+        sportList.add(sport);
+        
+        Mockito.when(sportRepository.findAll()).thenReturn(sportList);
+        List<Listable> newList = sportService.getAllSport();
+         
+        assertEquals(id, newList.get(0).getId());
+        assertEquals(name, newList.get(0).getName());
     }
 }
