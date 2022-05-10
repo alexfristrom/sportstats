944a73055b24146eb18408dd046e1eeb2efd6126
Ann Johansson <ann.j@live.se>
2022-05-10 11:29:34 +0200

    Tester för backlog nr 14 & 15 är klara. Fixat Game domain test

--- Domain/src/test/java/tests/domain/GameTest.java
+++ Domain/src/test/java/tests/domain/GameTest.java
@@ -8,6 +8,9 @@ import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import sportstats.domain.Game;
+import sportstats.domain.Result;
+import sportstats.domain.Season;
+import sportstats.domain.Team;
 import sportstats.handler.DateHandler;
 
 /**
@@ -18,77 +21,77 @@ import sportstats.handler.DateHandler;
  */
 public class GameTest {
 
-//    @Test
-//    public void homeTeamTest() {
-//        Game game = new Game();
-//        Long id = 1L;
-//
-//        game.setHomeTeamId(id);
-//        assertEquals(id, game.getHomeTeamId());
-//    }
-//
-//    @Test
-//    public void awayTeamTest() {
-//        Game game = new Game();
-//        Long id = 1L;
-//
-//        game.setAwayTeamId(id);
-//        assertEquals(id, game.getAwayTeamId());
-//    }
-//
-//    @Test
-//    public void seasonIdTest() {
-//        Game game = new Game();
-//        Long id = 1L;
-//
-//        game.setMatchSeasonId(id);
-//        assertEquals(id, game.getMatchSeasonId());
-//    }
-//
-//    @Test
-//    public void resultIdTest() {
-//        Game game = new Game();
-//        Long id = 1L;
-//
-//        game.setMatchResultId(id);
-//        assertEquals(id, game.getMatchResultId());
-//    }
+    @Test
+    public void homeTeamTest() {
+        Game game = new Game();
+        Team team = new Team();
 
-//    @Test
-//    public void roundTest() {
-//        Game game = new Game();
-//        Byte rounds = 1;
-//
-//        game.setMatchRound(rounds);
-//        assertEquals(rounds, game.getMatchRound());
-//    }
-//
-//    @Test
-//    public void dateTest() {
-//        DateHandler handler = new DateHandler();
-//        byte hour = 20;
-//        byte minute = 15;
-//        byte second = 0;
-//        handler.addTime(hour, minute, second);
-//        short year = 2022;
-//        byte month = 5;
-//        byte day = 15;
-//        handler.addDate(year, month, day);
-//
-//        Game game = new Game();
-//        game.setMatchDate(handler.getDateTime());
-//
-//        LocalDateTime refDate = LocalDateTime.of(year, month, day, hour, minute, second);
-//        assertEquals(refDate, game.getMatchDate());
-//    }
-//
-//    @Test
-//    public void spectatorTest() {
-//        Game game = new Game();
-//        Long spectators = 100L;
-//
-//        game.setMatchSpectators(spectators);
-//        assertEquals(spectators, game.getMatchSpectators());
-//    }
+        game.setHomeTeam(team);
+        assertEquals(team, game.getHomeTeam());
+    }
+
+    @Test
+    public void awayTeamTest() {
+        Game game = new Game();
+        Team team = new Team();
+
+        game.setAwayTeam(team);
+        assertEquals(team, game.getAwayTeam());
+    }
+
+    @Test
+    public void seasonTest() {
+        Game game = new Game();
+        Season season = new Season();
+
+        game.setSeason(season);
+        assertEquals(season, game.getSeason());
+    }
+
+    @Test
+    public void resultTest() {
+        Game game = new Game();
+        Result result = new Result();
+
+        game.setResult(result);
+        assertEquals(result, game.getResult());
+    }
+
+    @Test
+    public void roundTest() {
+        Game game = new Game();
+        Byte rounds = 1;
+
+        game.setRound(rounds);
+        assertEquals(rounds, game.getRound());
+    }
+
+    @Test
+    public void dateTest() {
+        DateHandler handler = new DateHandler();
+        byte hour = 20;
+        byte minute = 15;
+        byte second = 0;
+        handler.addTime(hour, minute, second);
+        short year = 2022;
+        byte month = 5;
+        byte day = 15;
+        handler.addDate(year, month, day);
+
+        Game game = new Game();
+        game.setDate(handler.getDateTime());
+
+        LocalDateTime refDate = LocalDateTime.of(year, month, day, hour, minute, second);
+        assertEquals(refDate, game.getDate());
+    }
+
+    @Test
+    public void spectatorTest() {
+        Game game = new Game();
+        Long spectators = 100L;
+
+        game.setSpectators(spectators);
+        assertEquals(spectators, game.getSpectators());
+    }
 
 }
--- Service/src/test/java/GameServiceTest.java
+++ Service/src/test/java/GameServiceTest.java
@@ -5,8 +5,7 @@
 
 import java.util.ArrayList;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import static org.mockito.ArgumentMatchers.any;
@@ -16,6 +15,7 @@ import sportstats.domain.Game;
 
 import sportstats.repository.GameRepository;
 import sportstats.service.GameService;
+import sportstats.service.util.GameByTeam;
 
 /**
  *
@@ -74,6 +74,82 @@ public class GameServiceTest {
 
         assertTrue(gameService.getAllGame().get(0) == game);
         assertTrue(gameService.getAllGame().get(1) == game2);
+
+    }
+
+    /**
+     * Testing behaviour of listAllByTeam method in GameService
+     */
+    @Test
+    public void testlistAllByTeam() {
+        mockSetup();
+        Game game = new Game();
+        Game game2 = new Game();
+
+        game.setSpectators(2L);
+        game2.setSpectators(3L);
+
+        List<Game> gameList = new ArrayList();
+        gameList.add(game);
+        gameList.add(game2);
+        Long id = 1L;
+
+        Mockito.when(gameRepository.listAllByTeam(id)).thenReturn(gameList);
+        List<GameByTeam> result = gameService.listAllByTeam(id);
+        assertFalse(result.isEmpty());
+        assertEquals(game.getSpectators(), result.get(0).getSpectators());
+        assertEquals(game2.getSpectators(), result.get(1).getSpectators());
+
+    }
+
+    /**
+     * Testing behaviour of listAwayByTeam method in GameService
+     */
+    @Test
+    public void testlistAwayByTeam() {
+        mockSetup();
+        Game game = new Game();
+        Game game2 = new Game();
+
+        game.setSpectators(2L);
+        game2.setSpectators(3L);
+
+        List<Game> gameList = new ArrayList();
+        gameList.add(game);
+        gameList.add(game2);
+        Long id = 1L;
+
+        Mockito.when(gameRepository.listAwayByTeam(id)).thenReturn(gameList);
+
+        List<GameByTeam> result = gameService.listAwayByTeam(id);
+        assertFalse(result.isEmpty());
+        assertEquals(game.getSpectators(), result.get(0).getSpectators());
+        assertEquals(game2.getSpectators(), result.get(1).getSpectators());
+
+    }
+
+    /**
+     * Testing behaviour of listHomeByTeam method in GameService
+     */
+    @Test
+    public void testlistHomeByTeam() {
+        mockSetup();
+        Game game = new Game();
+        Game game2 = new Game();
+
+        game.setSpectators(2L);
+        game2.setSpectators(3L);
+
+        List<Game> gameList = new ArrayList();
+        gameList.add(game);
+        gameList.add(game2);
+        Long id = 1L;
+
+        Mockito.when(gameRepository.listHomeByTeam(id)).thenReturn(gameList);
+        List<GameByTeam> result = gameService.listHomeByTeam(id);
+        assertFalse(result.isEmpty());
+        assertEquals(game.getSpectators(), result.get(0).getSpectators());
+        assertEquals(game2.getSpectators(), result.get(1).getSpectators());
 
     }
 
